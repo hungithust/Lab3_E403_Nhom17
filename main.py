0d91b1e2a862f54a492050483f3d09408c5de53b
tung <tung.nx20042024@gmail.com>
2026-04-06 15:12:58 +0700

    initial commit

--- main.py
+++
@@ -1,8 +0,0 @@
-from src.agent.agent import ReActAgent
-from src.core.gemini_provider import GeminiProvider
-
-llm = GeminiProvider(api_key="")
-
-response = llm.generate("Hello")
-
-print(response)

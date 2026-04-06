a552deff7c177c54e79660eea46d75b4066f8e7c
tung <tung.nx20042024@gmail.com>
2026-04-06 15:06:10 +0700

    chatbot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.agent.agent import ReActAgent
 from src.core.gemini_provider import GeminiProvider
 
-llm = GeminiProvider(api_key="AIzaSyChmJRJ6Oy-8uw22QShs3C7Oc24yiplUEo")
+llm = GeminiProvider(api_key="")
 
 response = llm.generate("Hello")
 
